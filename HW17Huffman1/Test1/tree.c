455423b3218daffd61bcc0900e595a90bd707232
Mohmmad Alwakeel <malwake@purdue.edu>
2021-02-03 22:48:51 -0500

    HW17

---
+++ HW17Huffman1/Test1/tree.c
@@ -0,0 +1,59 @@
+/*
+DO NOT MODIFY THIS FILE
+*/
+#include "tree.h"
+#include "utility.h"
+
+TreeNode * TreeNode_create(char val, int freq)
+{
+  TreeNode * tn = malloc(sizeof(TreeNode));
+  tn -> left = NULL;
+  tn -> right = NULL;
+  tn -> value = val;
+  tn -> freq = freq;
+  return tn;
+}
+
+TreeNode * Tree_merge(TreeNode * becomesLeftChild, TreeNode * becomesRighChild)
+{
+  TreeNode * tn = malloc(sizeof(TreeNode));
+  tn -> left = becomesLeftChild;
+  tn -> right = becomesRighChild;
+  tn -> value = 0; // do not care
+  tn -> freq = becomesLeftChild -> freq + becomesRighChild -> freq;
+  return tn;
+}
+
+// post-order
+void Tree_print(TreeNode * tn)
+{
+  if (tn == NULL)
+    {
+      return;
+    }
+  TreeNode * lc = tn -> left;  // left child
+  TreeNode * rc = tn -> right; // right child
+  Tree_print(lc);
+  Tree_print(rc);
+
+
+  if ((lc == NULL) && (rc == NULL))
+    {
+      // a leaf node
+      // fprintf(outfptr,"value = %d, '%c'", tn -> value, tn -> value);
+      printf("1%c", tn -> value);
+      return;
+    }
+  printf("0");
+}
+
+void Tree_destroy(TreeNode * tn)
+{
+  if (tn == NULL)
+    {
+      return;
+    }
+  Tree_destroy(tn -> left);
+  Tree_destroy(tn -> right);
+  free (tn);
+}
---
+++ HW17Huffman1/Test2/huffman.h~
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct _TreeNode {
+	long int amount;
+	int ascii;
+	struct _TreeNode* left;
+	struct _TreeNode* right;
+} TreeNode;
+
+typedef struct _Node {
+   struct _Node* next;
+   struct _TreeNode* tree;
+} Node;
+
+void _occurances_file();
+long int _ascii_occurances(char, FILE*);
+void _pq_file();
+void _print_node();
+void _pq_enqueue();
+void _destroy_node();
+Node * _pq_dequeue();
+void output_file_1();
+void _print_output1();
+void _destroy_tree_node();
+void post_order();
+void output_file_2();
+void binary(char**, char);
+void output_file_3();
